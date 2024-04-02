6476bf52a0573d8885b6fab6da32825a5a670de7
ymarting <ys.martin@upm.es>
2024-04-02 17:02:43 +0200

    TFGApplication

---
+++ src/main/java/com/example/demo2/TFGApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TFGApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TFGApplication.class, args);
+	}
+
+}
